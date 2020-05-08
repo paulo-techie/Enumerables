759ee5598cbede58a38d6ddc7d83d9b354cb8f57
paulo-techie <paulo.techie@gmail.com>
2020-05-08 07:52:21 -0600

    Validate all enumerables

--- main.rb
+++ main.rb
@@ -1,90 +1,95 @@
-
 module Enumerable
-    
+
     def my_each
-        return self.size if !block_given?
-        for item in self
-            yield(item)
-        end
+    unless !block_given?
+      for item in self
+        yield(item)
+      end
     end
-    
+      return self
+    end
+  
     def my_each_with_index
-        return self.size if !block_given?
-        for item_index in (0...self.size)
-            yield(self[item_index], item_index)
-        end
+    unless !block_given?
+      for item_index in (0...self.size)
+        yield(self[item_index], item_index)
+      end
     end
-
+      return self
+    end
+  
     def my_select
-        return self.size if !block_given?
-        new_items = []
-        my_each do |item|
-            new_items.push(item) if yield(item)
-        end
-        new_items
+    unless !block_given?
+      new_items = []
+      my_each do |item|
+        new_items.push(item) if yield(item)
+      end
+      return new_items
     end
-
+      return self
+    end
+  
     def my_all?
-        return true if !block_given?
-        items_exist = true
-        my_each do |item|
-                items_exist = false if !yield(item)
-        end
-        items_exist
+    return true if !block_given?
+    items_exist = true
+      my_each do |item|
+            items_exist = false if !yield(item)
+      end
+    items_exist
     end
-
-    def my_any?
-        any_item = false
-        my_each do |item|
-            any_item = true if yield(item)
-        end
-        any_item
-    end
-
-    def my_none?
-        item_none = false
-        my_each do |item|
-            item_none = true if !yield(item)
-        end
-        item_none
-    end
-
+  
     def my_count
-        return self.size if !block_given?
-        count = 0
+      return self.size if !block_given?
+      count = 0
+      my_select do |item|
+          count+=1 if yield(item)
+      end
+      count
+    end
+  
+    def my_any?
+      result = false
+      my_each do |item|
+          result = true if yield(item)
+      end
+      result
+    end
+  
+    def my_none?
+      result = true
+      unless !block_given?
         my_each do |item|
-            count+=1 if yield(item)
+            result = false if yield(item)
         end
-        count
+      end
+      return result
     end
-
+  
     def my_map(&proc)
-        return self.size if !block_given?
-        new_array = []
-        my_select { |item| new_array << proc.call(item)}
-        new_array
+      return self.size if !block_given?
+      new_array = []
+      my_select { |item| new_array << proc.call(item)}
+      new_array
     end
+  
 
-    def my_inject initial=nil
-       sum = start
-       for item_index in (0...self.size)
-            sum = yield(sum, self[item_index])
-       end
-       sum
+    def my_inject initial=0
+        sum = initial
+        item_index = 0
+         
+        sum = self[0] if initial ==0
+        item_index =1 if initial ==0
+   
+        for item_index in (item_index...self.size)
+              sum = yield(sum, self[item_index])
+        end
+        sum
+      
     end
-
-    def multiply_els
-        array.my_inject { |sum, item| sum * item }
-    end
-
-end
-# testers
-
-
- ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_each { |friend| puts "Hello, " + friend }
-
-  ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_each_with_index { |friend, index| puts "Hello, " + friend + " " + index.to_s}
-
-   ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun'].my_select { |friend| puts "Hello, " + friend }
-
-end
+  
+  end
+    
+  def multiply_els (array)
+    array.my_inject { |sum, item| sum * item }
+  end
+  
